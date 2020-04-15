d43bdb16665207cb8a1114e180a0cf2d1451c20b
MackRoe <elaine.music@students.makeschool.com>
2020-04-15 13:58:14 -0700

    problem: twosum, solution

---
+++ twosum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # subtract the first number from target to simplify the search process
+        x_num = target - nums[0]
+        for i in range(1, len(nums)):
+            # use range function to iterate through remaining list indexes
+            if nums[i] == x_num:
+                # if the value at list index matches x_num, return the indexes
+                # of the two values
+                return (0, i)
