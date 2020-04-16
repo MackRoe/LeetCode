ce64a9898cce4a3c4aa6c460e1ddfa7ca81308ee
MackRoe <elaine.music@students.makeschool.com>
2020-04-16 08:31:30 -0700

    continue filling in the communication steps comment verbage

--- twosum.py
+++ twosum.py
@@ -1,14 +1,32 @@
 # Communication Steps
 
 # - The Problem
+#       Find the indexes of two numbers in a list of integers, that when added
+#   together result in a certain prespecified sum.
 
 # - Clarifiying Questions
+#       The problem was stated clearly enough and requires no clarifying
+#   questions.
 
 # - Assumptions
+#       The numbers will be in numeric format and be of either integer or float
+#   types.
 
 # - Think out loud
 # -- Brainstorm Solutions
+#        One solution would be to step through the list, using some kind of
+#   loop to iterate through the values stored at each index, add them together,
+#   then check if the sum matches the target value.
+#       Another solution would be to subtract the value found at the first
+#   index then loop through the remaining indexes to find a value that matches
+#   the value calculated from the subtraction.
+
 # -- Explain Rationale
+#      A rationale is set of reasons or a logical basis for a course of action
+#   or a particular belief. Any solution that solves the problem accurately has
+#   value. A solution that solves it faster, using the least amount of storage
+#   and processing power, is better.
+
 # -- Discuss Tradeoffs
 # -- Suggest Improvements
 
